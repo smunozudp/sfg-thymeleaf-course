1991f7d542aa8757985a0c804a54a66fab71a14b
smunoz <s.munozeyquem2@gmail.com>
2020-12-06 09:06:37 -0300

    proyecto base

---
+++ src/main/java/com/beyondprogramming/SfgThymeleafCourseApplication.java
@@ -0,0 +1,13 @@
+package com.beyondprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgThymeleafCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgThymeleafCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beyondprogramming/SfgThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyondprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
